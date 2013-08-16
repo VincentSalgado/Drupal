c6ded3b130bb41d5dd7e4a3484588181f4937f66
Adam Urich <adam.urich@azdoa.gov>
2013-08-16 13:12:07 -0700

    Adding AZ Neighborhood and AZ Employee Search modules. Added AZ Agency shell folder.

---
+++ Modules/AZ Employee/employee.js
@@ -0,0 +1,8 @@
+(function($) {
+	
+	$(document).ready(function() {		
+		
+		$("#example").dataTable();
+        	//$("#example").hover(function(){alert('hey!!');});             
+    });
+})(jQuery);
