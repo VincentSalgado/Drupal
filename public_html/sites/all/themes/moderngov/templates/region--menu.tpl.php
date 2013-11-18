e8fe3e574aacd4d57e0f66a96dbf524d7e8b1901
Adam Urich <adam.urich@azdoa.gov>
2013-11-18 13:43:11 -0700

    Removed public_html.

--- public_html/sites/all/themes/moderngov/templates/region--menu.tpl.php
+++
@@ -1,8 +0,0 @@
-<div<?php print $attributes; ?>>
-  <div<?php print $content_attributes; ?>>
-    <nav class="navigation">
-      <?php //print render($dropdown_menu); ?>
-    <?php print $content; ?>
-    </nav>
-  </div>
-</div>
